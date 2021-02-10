5c6c5ae143b0e1712ecfe4a23e3c5087e61870cc
Unnikrishnan Nair, Rahul <rahulunair@gmail.com>
2021-02-10 14:00:34 -0800

    add aws cloud push

--- src/cloudstore/aws.py
+++ src/cloudstore/aws.py
@@ -1,4 +1,5 @@
 import json
+from os import MFD_ALLOW_SEALING
 import sys
 
 import boto3
@@ -13,8 +14,34 @@ from cloudstore.logger import logger
 class AWSStore(CloudStore):
     """aws cloud store."""
 
-    def upload(self, bucket: str, file) -> str:
-        pass
+    def __init__(self):
+        super().__init__()
+        try:
+            AWSStore.client = boto3.client("s3")
+            self.client = AWSStore.client
+        except aws_error as e:
+            logger.error("cannot login to aws")
+            logger.info("set credentials using environment variables")
+            logger.info(
+                "please see: https://boto3.amazonaws.com/v1/documentation/api/latest/guide/credentials.html#environment-variables"
+            )
+
+    def upload(self, bucket: str, file_name) -> str:
+        """aws upload file to aws."""
+        try:
+            self.client.upload_file(file_name, bucket, file_name)
+        except aws_error as e:
+            logger.error("client error: {}".format(e))
+        logger.info("file {} upload to s3".format(file_name))
 
     def download(self, bucket, object_name, file_name):
-        pass
+        """download file from s3 given an object name and bucket."""
+        try:
+            with open(file_name, "wb") as fh:
+                try:
+                    self.client.download_file(bucket, object_name, file_name)
+                except aws_error as e:
+                    logger.error("client error: {}".format(e))
+        except IOError as e:
+            logger.error("io error: {}".format(e))
+        logger.info("object: {} downloaded to file: {}".format(object_name, file_name))
