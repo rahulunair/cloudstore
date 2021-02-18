4dec8cd6df4f03a8cfeffa7a91c3cfcea1aa9037
Unnikrishnan Nair, Rahul <rahulunair@gmail.com>
2021-02-18 14:38:00 -0800

    remove api.py and data folder

--- src/cloudstore/api.py
+++
@@ -1,7 +0,0 @@
-from cloudstore.cloud_store import store
-
-# store = store("gcp")
-#store = store("aws")
-store = store("azure")
-store.upload(bucket="test3bucketunrahul", file_name="data/test_file.txt")
-store.download("test3bucketunrahul", "data/test_file.txt", "sds.txt")
